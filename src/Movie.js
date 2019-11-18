4f6173dd08aeeb3957530ba8210eebcbca633eaf
ckdwns9121 <40492343+ckdwns9121@users.noreply.github.com>
2019-11-18 13:12:28 +0900

    Update Movie.js

--- src/Movie.js
+++ src/Movie.js
@@ -2,39 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './Movie.css';
 
-// class Movie extends Component{
-//     //각 props의 타입 명세 해주기
-//     static propTypes={
-//         title: PropTypes.string.isRequired,
-//         poster: PropTypes.string.isRequired
-//     }
 
-//     render(){
-//         return(
-//             <div>
-//             <MoviePoster poster={this.props.poster}/>
-//             <h1>{this.props.title}</h1>
-//             </div>
-//         );
-//     }
-// }
-
-// class MoviePoster extends Component{
-//     static propTypes={
-//         poster:PropTypes.string.isRequired
-//     }
-//     render(){
-//         console.log(this.props);
-//         return(
-//             <img src={this.props.poster}></img>
-//         );
-
-//     }
-// }
-//return을 해주기 위해 존재하는 멍청한 컴포넌트
-
-  //            <MoviePoster poster={poster} alt={title}/>
-// function Movie({title,poster,genres,synopsis}){
 const Movie =({title,poster,genres,synopsis}) =>{
     return(
         <div className="Movie">
@@ -53,7 +21,7 @@ const Movie =({title,poster,genres,synopsis}) =>{
         </div>
     );
 }
-//클래스 형태로 만들어진 컴포넌트는 반드시 render가 있어야한다.
+
 function MoviePoster({poster,alt}){
     return(
         <img src={poster} alt="none" title={alt} class="Movie_Poster"></img>
