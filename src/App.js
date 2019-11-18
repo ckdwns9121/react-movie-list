e25bab940498355de93e930e8e0d142ffda6e277
ckdwns9121 <40492343+ckdwns9121@users.noreply.github.com>
2019-11-18 13:13:16 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,21 +2,9 @@ import React,{ Component } from 'react';
 import './App.css';
 import Movie from './Movie';
 
- //리스트는 무조건 고유 key값을 index번호로 명시해줘야함
- 
 class App extends Component  {
   state ={};
 
-   //didmount는 render이후에 실행되는 메소드
-   // 무비리스트를 들고오기위한 준비
-
-   /*
-
-  didmount 에서 getmoive 해준다
-  getmoive 에서 callApi 불러오고 state(result) 에 set해준다
-
-
-   */
    componentDidMount(){
     this._getMovies();
    }
@@ -35,7 +23,6 @@ class App extends Component  {
       return movies;
    }
    
-  //상태를 설정하고 비동기로 받아온 데이터를 셋팅
    _getMovies = async () =>{
       const result = await this._callApi();
       this.setState({result});
@@ -43,16 +30,14 @@ class App extends Component  {
 
    }
   
-   //json 으로 비동기식 데이터를 받아오는 함수
    _callApi = ()=>{
     return fetch('https://yts.lt/api/v2/list_movies.json?sort_by=rating')
    .then(response => response.json())
-   .then(json => json.data.movies) //json데이터에 movie 라는 데이터를 기준으로 들고온다
-  //  .catch(err=>console.log(err))
+   .then(json => json.data.movies)
+    .catch(err=>console.log(err))
    
   }
-   
-   //무비.js에서 그렸던 컴포넌트를 그리는렌더링ㅋ
+
   render(){
     const {result} = this.state;
     return (
